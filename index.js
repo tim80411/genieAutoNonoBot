2a52b63a0156519d7070af371049c5445d035805
Timothy Liao <tim80411@gmail.com>
2022-06-08 20:48:56 +0800

    structure: commands deploy and event listen

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-// Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 
+const commands = require('commands/entry');
+const logger = require('lib/logger');
 
 const token = process.env.DISCORD_TOKEN;
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// @ts-ignore
+client.commands = commands;
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-  console.log('Ready!');
+  logger.info({ msg: 'Ready!' });
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    logger.error({ msg: 'Error!', error });
+
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
 // Login to Discord with your client's token
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+import * as discord from 'discord.js'
+
+declare module 'discord.js' {
+  export interface Client {
+    commands: Collection<unknown, any>
+  }
+}
